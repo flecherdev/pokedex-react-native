470e06ffb50b9e3d08615d6c86afdc6e332a9aca
Ezequiel A. Freire <ezequiel.a.freire@gmail.com>
2024-01-30 10:44:01 -0300

    added icon in nav

--- src/screen/Pokemon.tsx
+++ src/screen/Pokemon.tsx
@@ -4,6 +4,7 @@ import Type from '../components/Pokemon/Type';
 import {useEffect, useState} from 'react';
 import {getPokemonDetailsApi} from '../api/pokeapi';
 import Stats from '../components/Pokemon/Stats';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 const Pokemon = (props: any) => {
   const {
@@ -13,7 +14,20 @@ const Pokemon = (props: any) => {
   } = props;
   const [pokemon, setPokemon] = useState();
 
-  console.log('route: ', route.params.pokemon);
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => null,
+      headerLeft: () => (
+        <Icon
+          name="arrow-left"
+          color="#fff"
+          size={20}
+          style={{marginLeft: 20}}
+          onPress={navigation.goBack}
+        />
+      ),
+    });
+  }, [navigation, params]);
 
   useEffect(() => {
     (async () => {
@@ -29,13 +43,13 @@ const Pokemon = (props: any) => {
   if (!pokemon) return null;
 
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <Header pokemon={pokemon} />
-        <Type type={pokemon} />
-        <Stats stat={pokemon} />
-      </ScrollView>
-    </SafeAreaView>
+    // <SafeAreaView>
+    <ScrollView>
+      <Header pokemon={pokemon} />
+      <Type type={pokemon} />
+      <Stats stat={pokemon} />
+    </ScrollView>
+    // </SafeAreaView>
   );
 };
 
